52391ec2cbdb5d6a977130744a74d72928094cf2
vinnieOrdobas <viniordobas@icloud.com>
2024-08-04 18:28:56 +0100

    create predictors class and unit test

---
+++ models/predictors.py
@@ -0,0 +1,46 @@
+import joblib
+import os
+import numpy as np
+
+from sklearn.base import is_classifier
+
+class Predictors:
+    def __init__(self, model_dir='models/predictors'):
+        '''
+        Initialize models and output prediction
+        '''
+
+        self.model_dir = model_dir
+        self.models = self._load_models()
+
+    def _load_models(self):
+        '''
+        Load trained models using joblib
+        '''
+
+        models = {}
+        for filename in os.listdir(self.model_dir):
+            if filename.endswith('.pkl'):
+                model_name = filename.replace('.pkl', '')
+                model_path = os.path.join(self.model_dir, filename)
+                models[model_name] = joblib.load(model_path)
+        return models
+    
+    def predict(self, X):
+        '''
+        Make predictions given new observation, through soft voting
+        '''
+
+        model_predictions = []
+
+        for model_name, model in self.models.items():
+            if is_classifier(model):
+                preds = model.predict_proba(X)
+                model_predictions.append(preds)
+            
+        if not model_predictions:
+            raise ValueError('No predictions found')
+        
+        avg_predictions = np.mean(model_predictions, axis=0)
+
+        return np.argmax(avg_predictions, axis=1)
---
+++ tests/test_predictors.py
@@ -0,0 +1,67 @@
+import pytest
+import numpy as np
+import joblib
+from unittest.mock import MagicMock, patch
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+from predictors import Predictors
+
+@pytest.fixture
+def stub_mlr_model():
+    model = LogisticRegression()
+    model.predict_proba = MagicMock(return_value= np.array([[0.2, 0.5, 0.3]]))
+    return model
+
+@pytest.fixture
+def stub_rf_model():
+    model = RandomForestClassifier()
+    model.predict_proba = MagicMock(return_value=np.array([[0.1, 0.6, 0.3]]))
+    return model
+
+@pytest.fixture
+def stub_gbc_model():
+    model = GradientBoostingClassifier()
+    model.predict_proba = MagicMock(return_value=np.array([[0.3, 0.4, 0.3]]))
+    return model
+
+@pytest.fixture
+def sample_data():
+    """
+    Fixture to provide sample input data for the tests.
+    """
+    return np.array([[5.1, 3.5, 1.4, 0.2]])
+
+@pytest.fixture
+def mock_load_models(stub_mlr_model, stub_rf_model, stub_gbc_model):
+    with patch('os.listdir') as mock_listdir, patch('joblib.load') as mock_load:
+        mock_listdir.return_value = ['mlr_model.pkl', 'rf_model.pkl', 'gbc_model.pkl']
+        mock_load.side_effect = [stub_mlr_model, stub_rf_model, stub_gbc_model]
+        yield mock_load
+
+@pytest.fixture
+def predictor_instance(mock_load_models):
+    # Create an instance of Predictors
+    return Predictors()
+
+def test_load_models(predictor_instance, stub_mlr_model, stub_rf_model, stub_gbc_model):
+
+    expected_models = {
+        'mlr_model': stub_mlr_model,
+        'rf_model': stub_rf_model,
+        'gbc_model': stub_gbc_model
+    }
+
+    assert predictor_instance.models == expected_models
+
+def test_predict(predictor_instance, sample_data):
+    
+    prediction = predictor_instance.predict(sample_data)
+
+    expected_proba = np.array([[0.2, 0.5, 0.3], [0.1, 0.6, 0.3], [0.3, 0.4, 0.3]])
+    expected_avg_proba = np.mean(expected_proba, axis=0)
+    expected_class = np.argmax(expected_avg_proba)
+    assert np.array_equal(prediction, [expected_class]), f"Expected {[expected_class]} but got {prediction}"
+
+if __name__ == "__main__":
+    pytest.main()
